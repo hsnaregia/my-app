95ca83577c011b5af3ad5b480885420c4e53b496
hsnaregia <hsnaregia@gmail.com>
2026-07-17 18:28:58 +0330

    some bug fixes

--- src/components/ui/TaskCard.tsx
+++ src/components/ui/TaskCard.tsx
@@ -1,25 +1,18 @@
 import type { Task } from '../../../types/task';
 
 interface TaskCardProps {
-  task:Task;
-  onEdit?: (task: Task) => undefined|void|string;
+  task: Task;
+  onEdit?: (task: Task) => undefined | void | string;
 }
 
-export const TaskCard = (
-{task,onEdit}
-: TaskCardProps) => {
+export const TaskCard = ({ task, onEdit }: TaskCardProps) => {
   return (
     <div className="bg-white border rounded-lg p-3 flex flex-col gap-2">
       <div className="flex justify-between items-center">
         <h3 className="text-sm font-medium">{task.title}</h3>
 
         <div className="flex gap-2">
-          <span
-            className="cursor-pointer"
-            onClick={() =>
-              onEdit?.(task)
-            }
-          >
+          <span className="cursor-pointer" onClick={() => onEdit?.(task)}>
             ✏️
           </span>
 
